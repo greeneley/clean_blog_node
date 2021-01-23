75073d5939ac98710b932ae224d05e0ea3e56065
greeneley <anhchangtoanhoc97@gmail.com>
2021-01-23 13:06:31 +0700

    use EJS engine

--- blog/index.js
+++ blog/index.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const app = new express()
 const path  = require('path')
+const ejs = require('ejs')
 
 app.use(express.static('public'))
+app.set('view engine', 'ejs')
 
 app.listen(4000, () => {
     console.log('App listening on port 4000')
 })
 
 app.get('/', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    // response.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    response.render('index')
+})
+// app.get('/', (request, response) => {
+// })
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+})
+
+app.get('/post', (req, res) => {
+    res.render('post')
 })
