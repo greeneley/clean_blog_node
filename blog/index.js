de7dc8631ad022cc878a3f20472130cf166bc259
greeneley <anhchangtoanhoc97@gmail.com>
2021-01-26 00:01:25 +0700

    Need fix error page 46

--- blog/index.js
+++ blog/index.js
@@ -3,9 +3,13 @@ const app = new express()
 const path  = require('path')
 const ejs = require('ejs')
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const BlogPost = require('./models/BlogPost.js')
+
 
 mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true})
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
@@ -30,4 +34,16 @@ app.get('/contact', (req, res) => {
 
 app.get('/post', (req, res) => {
     res.render('post')
+})
+
+app.get('/posts/new', (req, res) => {
+    res.render('create')
+})
+
+app.post('/posts/store', (req,res) => {
+    console.log(req.body)
+    // res.redirect('/')
+    BlogPost.create(req.body, (error, blogpost) => {
+        res.redirect('/')
+    })
 })
