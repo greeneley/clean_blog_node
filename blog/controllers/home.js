061fab2a4eb90e2dad374a45367edfbae0f326e0
greeneley <anhchangtoanhoc97@gmail.com>
2021-02-06 21:24:26 +0700

    remove console log

--- blog/controllers/home.js
+++ blog/controllers/home.js
@@ -1,7 +1,7 @@
 const BlogPost = require('../models/BlogPost.js')
 module.exports = (req, res) => {
     BlogPost.find({}, function (error, posts) {
-        console.log(req.session)
+        // console.log(req.session)
         res.render('index', {
             blogposts: posts
         });
